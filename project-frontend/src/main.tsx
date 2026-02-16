6f20ed63fa344ecec106ace74ebfa9221e502f50
m4tch1na <abejaoui90@gmail.com>
2026-02-16 17:32:54 +0100

    Configure axios global baseURL

--- project-frontend/src/main.tsx
+++ project-frontend/src/main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import axios from "axios";
 import App from "./App";
+
+// Set global base URL for axios
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE || 'http://localhost:3000';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
